d67ec03323f965094290c8daf1663016011be7f7
alexrod94 <alexrod94@gmail.com>
2022-03-15 13:06:47 +0100

    noon

---
+++ mi-router/src/components/Article.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>Artículo del blog</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ mi-router/src/views/Blog.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h2>Blog</h2>
+
+    <!-- <input type="checkbox" value="checked" v-model="isChecked" /> -->
+    <button @click="showParagraph">Mostrar</button>
+    <h1 v-if="show">Conditional rendering is really cool!</h1>
+    <h1 v-else-if="counter">My counter is 3</h1>
+    <h1 v-else>Oh no</h1>
+  </div>
+</template>
+
+<script>
+import Article from "../components/Article.vue";
+export default {
+  components: {
+    Article,
+  },
+  data() {
+    return {
+      show: false,
+      counter: 3,
+    };
+  },
+  methods: {
+    showParagraph() {
+      this.show = !this.show;
+    },
+  },
+};
+</script>
+
+<style></style>
